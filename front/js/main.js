c460960a10e186d73b7a7092c6be0ea74743801a
Mari <mari@MacBook-Pro.local>
2024-12-09 14:01:22 +0200

    noios-padding

--- front/js/main.js
+++ front/js/main.js
@@ -1,16 +1,8 @@
 // padding no-ios
 document.addEventListener("DOMContentLoaded", function() {
-    let bannerLobbyLink = document.querySelector('.bannerLobby__link');
-    let bannerRightTop = document.querySelector('.bannerLobby__right-top');
-    let bannerRightBottom = document.querySelector('.bannerLobby__right-bottom');
-    let bannerLeftTop = document.querySelector('.bannerLobby__left-top');
-    let bannerRightLeft = document.querySelector('.bannerLobby__left-bottom');
+    let bannerLobbyBtn = document.querySelector('.bannerLobby__link');
 
     if (!/iPad|iPhone|iPod|Mac/.test(navigator.userAgent)) {
-        bannerLobbyLink.classList.add('noios-padding');
-        bannerRightTop.classList.add('noios-padding');
-        bannerRightBottom.classList.add('noios-padding');
-        bannerLeftTop.classList.add('noios-padding');
-        bannerRightLeft.classList.add('noios-padding');
+        bannerLobbyBtn.classList.add('noios-padding');
     }
 });
